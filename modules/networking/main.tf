45b20886893ca8bc198dec9395ff7e8e1fa1eaf5
Mohammad Youssef <89692190+MHDYoussef@users.noreply.github.com>
2025-06-10 13:58:39 +0400

    add networking module

---
+++ modules/networking/main.tf
@@ -0,0 +1,45 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "aks-subnet"
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "private_endpoint" {
+  name                 = "private-endpoint-subnet"
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "jumpbox" {
+  name                 = "jumpbox-subnet"
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_network_security_group" "jumpbox_nsg" {
+  name                = "nsg-jumpbox-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
